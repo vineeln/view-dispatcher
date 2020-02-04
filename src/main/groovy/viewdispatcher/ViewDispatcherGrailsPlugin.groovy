5abfb6bd9e4a92904bdd7af0bd130f3d5a77d41c
Vineel Nalla <vineeln@gmail.com>
2020-02-04 08:53:11 -0500

    3.3.7 >

--- src/main/groovy/viewdispatcher/ViewDispatcherGrailsPlugin.groovy
+++ src/main/groovy/viewdispatcher/ViewDispatcherGrailsPlugin.groovy
@@ -10,7 +10,7 @@ import grails.plugins.*
 class ViewDispatcherGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.3.11 > *"
+    def grailsVersion = "3.3.7 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
